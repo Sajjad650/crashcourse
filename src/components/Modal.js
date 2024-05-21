cdb61390afda0a6701e3aa7f90d33df19478b2d8
Sajjad <sajjad@pixarsart.com>
2024-05-21 19:46:06 +0500

    push code

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import classes from './Model.module.css';
-export default function Modal({children}) {
+import React from "react";
+import classes from "./Model.module.css";
+export default function Modal({ children }) {
   return (
     <>
-    <div className={classes.backdrop}>
-      
-    </div>
-    <dialog open className={classes.modal}>{children}</dialog>
+      <div className={classes.backdrop}></div>
+      <dialog open className={classes.modal}>
+        {children}
+      </dialog>
     </>
-  )
+  );
 }
--- src/components/NewPost.js
+++ src/components/NewPost.js
@@ -1,22 +1,24 @@
-import classes from './NewPost.module.css';
-
-
+import classes from "./NewPost.module.css";
 
 export default function NewPost(props) {
-
   return (
     <div>
       <form className={classes.form}>
-      <p>
-        <label htmlFor="body">Text</label>
-        <textarea id="body" required rows={3} onChange={props.onBodyChange} />
-      </p>
-      
-      <p>
-        <label htmlFor="name">Your name</label>
-        <input type="text" id="name" required onChange={props.onAuthorChange}/>
-      </p>
-    </form>
+        <p>
+          <label htmlFor="body">Text</label>
+          <textarea id="body" required rows={3} onChange={props.onBodyChange} />
+        </p>
+
+        <p>
+          <label htmlFor="name">Your name</label>
+          <input
+            type="text"
+            id="name"
+            required
+            onChange={props.onAuthorChange}
+          />
+        </p>
+      </form>
     </div>
-  )
+  );
 }
