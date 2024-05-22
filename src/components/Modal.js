fb2470efe51c9635b65c999d2947b8b170583465
Sajjad <sajjad@pixarsart.com>
2024-05-22 22:12:00 +0500

    all files changes

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -2,19 +2,19 @@ import React, { useEffect } from "react";
 import classes from "./Model.module.css";
 
 export default function Modal({ children, onClose }) {
-  // useEffect(() => {
-  //   const handleClickOutside = (event) => {
-  //     if (event.target.classList.contains(classes.backdrop)) {
-  //       onClose(); // Close the modal if clicked outside of it
-  //     }
-  //   };
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (event.target.classList.contains(classes.backdrop)) {
+        onClose(); // Close the modal if clicked outside of it
+      }
+    };
 
-  //   document.addEventListener("click", handleClickOutside);
+    document.addEventListener("click", handleClickOutside);
 
-  //   return () => {
-  //     document.removeEventListener("click", handleClickOutside);
-  //   };
-  // }, [onClose]);
+    return () => {
+      document.removeEventListener("click", handleClickOutside);
+    };
+  }, [onClose]);
 
   return (
     <>
